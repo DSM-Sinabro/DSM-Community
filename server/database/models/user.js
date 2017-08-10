0b450cdfa05fe52ee61829561cd46a47e32ad472
Terry Yun <nooheat1228@gmail.com>
2017-08-11 01:32:18 +0900

    [remove] useless remark

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -12,23 +12,6 @@ let schema = Schema({
     createdAt : { type : String, required : true, default : date.getDateTimeNow() }
 }, { collection : 'User'});
 
-// schema.statics.signUp = function(info, callback){
-//     let user = new this();
-//     user.uid = user.createUUID();
-//     user.cardinal = 2;
-//     user.code = 'dsm201646';
-//     user.id = 'test';
-//     user.password = '1234';
-//     user.save(function(err){
-//         if(err !== null) { // typeof err === "undefined"는 안됨, null은 object를 반환 :: 오류가 발생하든 안하든 object형임
-//             if(err.errmsg.includes('duplicate key')) console.log("duplicate key error");
-//             else console.log(err);
-//             callback(false);
-//         }
-//         else callback(true);
-//     });
-// };
-
 schema.methods.createUUID = function(){
     let coll = this;
     let d = new Date().getTime();
