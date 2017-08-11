03adf958206c55f3b05439ab593950d92da262e3
geni429 <geni429@gmail.com>
2017-08-11 11:52:28 +0900

    image로드 문제 해결

--- react-app/src/components/QwriteArea.js
+++ react-app/src/components/QwriteArea.js
@@ -9,25 +9,25 @@ class QwriteArea extends Component{
                 </div>
                 <div id="functions">
                     <div id="decoration">
-                        <img src="../imgs/code.png" alt="code"/>
-                        <img src="../imgs/bold.png" alt="bold"/>
-                        <img src="../imgs/italic.png" alt="italic"/>
-                        <img src="../imgs/underline.png" alt="underline"/>
+                        <img src={require("../imgs/code.png")} alt="code"/>
+                        <img src={require("../imgs/bold.png")} alt="bold"/>
+                        <img src={require("../imgs/italic.png")} alt="italic"/>
+                        <img src={require("../imgs/underline.png")} alt="underline"/>
                     </div>
                     <div id="align">
-                        <img src="../imgs/align_left.png" alt=""/>
-                        <img src="../imgs/align_center.png" alt=""/>
-                        <img src="../imgs/align_right.png" alt=""/>
-                        <img src="../imgs/align_justify.png" alt=""/>
+                        <img src={require("../imgs/align_left.png")} alt=""/>
+                        <img src={require("../imgs/align_center.png")} alt=""/>
+                        <img src={require("../imgs/align_right.png")} alt=""/>
+                        <img src={require("../imgs/align_justify.png")} alt=""/>
                     </div>
                 </div>
                 <div id="upload">
                     <span>업로드: </span>
                     <div>
-                        <img src="../imgs/picture.png" alt="picture"/>
+                        <img src={require("../imgs/picture.png")} alt="picture"/>
                     </div>
                     <div>
-                        <img src="../imgs/folder.png" alt="folder"/>
+                        <img src={require("../imgs/folder.png")} alt="folder"/>
                     </div>
                 </div>
                 <div id="Qwrite">
