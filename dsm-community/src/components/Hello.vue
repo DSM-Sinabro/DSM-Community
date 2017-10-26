892b2f0e6fed81c04f5819fa5d038fced22370e0
Ground-Silver0903 <kyn07077@gmail.com>
2017-10-26 23:27:17 +0900

    for pull

---
+++ dsm-community/src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>HelloWorld!</h1>
+      <div id = "helloDiv"></div>
+  </div>
+</template>
+
+
+<script>
+new Vue({
+    el : "#helloDiv",
+    name : "Hello"
+})
+Vue.component('Hello')
+</script>
+
+<style scoped>
+
+.helloDiv{
+    width : 100px;
+    height : 100px;
+    border : 1px solid black;
+}
+</style>
+
+
+
--- dsm-community/src/components/HelloWorld.vue
+++ dsm-community/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <hello />
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -21,13 +22,15 @@
 </template>
 
 <script>
+import Hello from './hello.vue'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
 }
 </script>
 
