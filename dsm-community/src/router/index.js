d05511c15589afbccbdcc2ca3bae05b23b7ec7be
Suyoung456 <rsy011203@gmail.com>
2017-11-06 19:29:41 +0900

    Create SignUpModal.vue

--- dsm-community/src/router/index.js
+++ dsm-community/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '../containers/Main'
 import Login from '../containers/Login'
+import SignUp from '../containers/SignUp'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
