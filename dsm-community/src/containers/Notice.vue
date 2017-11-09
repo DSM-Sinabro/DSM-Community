ccda5d80116d96037a79711cc1d221838b1e1713
Dowapdowari <ktwind123@daum.net>
2017-11-09 23:50:10 +0900

    notice part first add

--- dsm-community/src/containers/Notice.vue
+++ dsm-community/src/containers/Notice.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
   <notice-body />
+  <search-notice />
+  <main-notice />
   </div>
 </template>
 
 <script>
 import TopBar from '../components/TopBar'
 import NoticeBody from '../components/NoticeBody'
+import MainNotice from '../components/MainNotice'
+import SearchNoice from '../components/SearchNotice'
 
 export default {
   name: 'Main',
   components: {
     TopBar,
-    NoticeBody
+    NoticeBody,
+    SearchNoice,
+    MainNotice
   }
 }
 </script>
