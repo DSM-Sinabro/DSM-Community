37bdec5aecfe98ca68e5347940e4fb4adb6ff54f
HubCodes <krlrhkstk12@gmail.com>
2017-11-13 18:55:32 +0900

    [Swagger] Swagger 적용 완료

--- server/routes/freeboard/article.create.js
+++ server/routes/freeboard/article.create.js
@@ -3,6 +3,50 @@ let userDB = require('../../database/models/user');
 
 let Schema = require('mongoose').Schema;
 
+/**
+ * @swagger
+ * /freeboard:
+ *   post:
+ *     description: Creates a new article
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: title
+ *         description: Article title
+ *         in: body (json)
+ *         required: true
+ *         schema:
+ *           "title": String
+ * 		 - name: contents
+ * 		   description: Article inbody
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "contents": String
+ * 		 - name: tags
+ * 		   description: Tags for article
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "tags": [String]
+ * 		 - name: images
+ * 		   description: Images for article
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "images": [String]
+ * 		 - name: authorUid
+ * 		   description: Article uploader's UID
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "authorUid": String (length = 24)
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ * 		 500:
+ * 		   description: Internal server error
+ */
 let router = (req, res) => {
 	let title = req.body.title;
 	let contents = req.body.contents;
--- server/routes/freeboard/article.modify.js
+++ server/routes/freeboard/article.modify.js
@@ -3,6 +3,56 @@ let userDB = require('../../database/models/user');
 
 let Schema = require('mongoose').Schema;
 
+/**
+ * @swagger
+ * /freeboard:
+ *   put:
+ *     description: Updates an article
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: title
+ *         description: Article title
+ *         in: body (json)
+ *         required: true
+ *         schema:
+ *           "title": String
+ * 		 - name: contents
+ * 		   description: Article inbody
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "contents": String
+ * 		 - name: tags
+ * 		   description: Tags for article
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "tags": [String]
+ * 		 - name: images
+ * 		   description: Images for article
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "images": [String]
+ * 		 - name: authorUid
+ * 		   description: Article uploader's UID
+ * 		   in: body (json)
+ * 		   required: true
+ * 		   schema:
+ * 			  "authorUid": String (length = 24)
+ * 		 - name: Article ID
+ *         description: Article ID (Want to modify)
+ *         in: url (parameter)
+ *         required: true
+ *         schema:
+ *            /id: Number
+ *     responses:
+ *       201:
+ *         description: Successfully updated
+ * 		 500:
+ * 		   description: Internal server error
+ */
 let router = (req, res) => {
 	let id = req.params.id;
 	let title = req.body.title;
