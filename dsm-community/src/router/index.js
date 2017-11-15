7bcabd475d4a63b8c8999b0499ff45f948d64da6
Dowapdowari <ktwind123@daum.net>
2017-11-15 12:12:43 +0900

    오류있음

--- dsm-community/src/router/index.js
+++ dsm-community/src/router/index.js
@@ -4,6 +4,7 @@ import Main from '../containers/Main'
 import SignUp from '../containers/SignUp'
 import Notice from '../containers/Notice'
 import Login from '../containers/Login'
+import ChangePw from '../components/TopBar/ChangePw'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/changepw',
+      name: 'changePw',
+      component: ChangePw
     }
   ]
 })
