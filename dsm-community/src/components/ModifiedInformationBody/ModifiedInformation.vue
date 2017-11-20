ed0cdb006e87d1a2bb735ebdbec0be031341a0c1
Park-haebin <qorgkq356@gmail.com>
2017-11-20 23:11:08 +0900

    회원정보 수정

--- dsm-community/src/components/ModifiedInformationBody/ModifiedInformation.vue
+++ dsm-community/src/components/ModifiedInformationBody/ModifiedInformation.vue
@@ -1,43 +1,45 @@
 <template>
-  <div id="password">
-    <span id="facebook">facebook</span>
-    <input type="password">
-    <p><button type="button">확인</button></p>
+  <div id="modify">
+    <table>
+      <tr>
+        <td>이름</td>
+        <td><input type="text"></td>
+      </tr>
+      <tr>
+        <td>email</td>
+        <td><input type="text"></td>
+      </tr>
+      <tr>
+        <td>비밀번호 변경</td>
+        <td><input type="password"></td>
+      </tr>
+      <tr>
+        <td>비밀번호 확인</td>
+        <td><input type="password"></td>
+      </tr>
+      <button type="button">닫기</button>
+      <button type="button">완료</button>
+    </table>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'ModifiedInformation',
+  name: 'ModifiedInformation'
 }
 </script>
 
 <style scoped>
 
-#password {
-  height: 20vh;
+#modify {
+  height: 30vh;
   text-align: center;
   border: 2px solid #D8D8D8;
   border-width: 1px 2px 2px 2px;
 }
 
-#text-password {
-  font-size: 1.2rem; 
-  line-height: 13vh;
-}
-
 input {
   border: 1px solid #000000;
   line-height: 2.3vh;
-}
-
-button {
-  height: 30px;
-  width: 50px;
-  border: 1px solid #5D5D5D;
-  font-size: 1rem;
-  color: #5D5D5D;
-  background-color: #ffffff;  
 }
 </style>
 
