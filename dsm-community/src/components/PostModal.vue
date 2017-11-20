0e38bac1464526141f48ca252da0f02a4759d4a9
Suyoung456 <rsy011203@gmail.com>
2017-11-20 20:29:00 +0900

    Creat PostModal

---
+++ dsm-community/src/components/PostModal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="all">
+        <title />
+        <writing />
+        <hashtag />
+        <show-hashtag />
+    </div>
+</template>
+
+<script>
+import Hashtag from './PostModal/Hashtag'
+import ShowHashtag from './PostModal/ShowHashtag'
+import Title from './PostModal/Title'
+import Writing from './PostModal/Writing'
+
+export default {
+  name: 'PostModals',
+  components: {
+    Hashtag,
+    ShowHashtag,
+    Title,
+    Writing
+  }
+}
+</script>
+
+<style>
+#all{
+    height: 500px;
+    width: 300px;
+    background-color: black;
+}
+</style>
