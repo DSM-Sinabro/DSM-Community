b6c787cfb2a067771dc9f86dca9f1d8710f86990
jieunjieun <kyn07077@gmail.com>
2017-11-21 17:18:29 +0900

    캘린더 잘 떠라

---
+++ dsm-community/src/components/MyPage/Recruit/Template.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class = "box">
+      <div class = "title">
+        <div class = "project_text">프로젝트 모집</div>
+        <div class = "underline"></div>
+      </div>
+
+      <table>
+          <tr v-for=" number in 4" :key = "number.id" class = "hello">
+              <td>{{ number }}: </td>
+              <td>title</td>
+              <td class = "functionButtons">
+                  <div class = "modify">수정</div>
+                  <div class = "delete">삭제</div>
+              </td>
+          </tr>          
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Template'
+}
+</script>
+
+<style scoped>
+.box{
+        width: 50%;
+        height: 50%; 
+        border: 1px solid black;
+        float: left;
+        vertical-align: middle;
+    }
+    .title{
+        margin: 15px 15px 15px 15px;
+    }
+
+    .underline{
+        margin: auto;
+        width: 98%;
+        height: 1px;
+        background-color: black;
+    }
+
+    table{
+        margin-left: 20px;
+        width: 90%;
+        border-spacing: 10px 10px;
+    }
+
+    .functionButtons{
+        float: right;
+        /* display: inline-block; */
+    }
+
+    .modify{
+        float: left;
+        border: 1px solid black;
+        margin-right: 5px;
+        font-size: 13px;
+    }
+    .delete{
+        float: left;
+        border: 1px solid black;
+        font-size: 13px;
+    }
+    
+</style>
