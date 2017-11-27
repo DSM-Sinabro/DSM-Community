ae2adb8458989ed651ea20e685dd9e66a9c6dd03
Park-haebin <qorgkq356@gmail.com>
2017-11-27 19:00:13 +0900

    writing 수정

--- dsm-community/src/components/PostBody.vue
+++ dsm-community/src/components/PostBody.vue
@@ -2,11 +2,13 @@
   <div>
     <top-bar />
     <div>
-    <h1 id="title">공지사항</h1>
-    <button id="writing" v-on:click="post">글쓰기</button>
+      <h1 id="title">공지사항</h1>
     </div>
-    <board-main />
     <board-search />
+    <board-main v-for="notice in notices" :notice="notice" :key="notice.id"/>
+    <div>
+      <button id="writing" v-on:click="post">글쓰기</button>
+    </div>
   </div>
 </template>
 
@@ -17,7 +19,7 @@ import BoardSearch from './BoardBody/BoardSearch'
 import BoardMain from './BoardBody/BoardMain'
 
 export default {
-  name: 'Main',
+  name: 'PostBody',
   components: {
     TopBar,
     BoardMain,
@@ -28,6 +30,17 @@ export default {
       this.$emit('post')
       console.log('post')
     }
+  },
+  data: function () {
+    return {
+      notices: [
+        {title: 'HELLO', image: '1234', name: '박해빈', date: '2017/11/08', views: '123', reply: '123', tag: '#HELLO', tag2: '#', tag3: '#'},
+        {title: '체육대회', image: '1234', name: '장필준', date: '2017/11/08', views: '123', reply: '123', tag: '#배구', tag2: '#줄다리기', tag3: '#예선'},
+        {title: 'DSM 공모전', image: '13123', name: '한능희', date: '2018/12/31', views: '123', reply: '123', tag: '#힘내라', tag2: '#영상제출', tag3: '#11.15'},
+        {title: '자치회 회의 초대', image: '123', name: '박해빈', date: '2010/12/12', views: '123', reply: '123', tag: '#윤정현', tag2: '#박해빈', tag3: '#자치회'},
+        {title: '학교 규정', image: '1232', name: '서현철', date: '2010/12/12', views: '123', reply: '122', tag: '#동복', tag2: '#사복안됨', tag3: '#춥다'}
+      ]
+    }
   }
 }
 </script>
@@ -36,10 +49,17 @@ export default {
 h1 {
   margin-top: 70px;
   margin-left: 28%;
-  float: left;
 }
-#writing{
-  width: 40px;
+
+#writing {
+  width: 67px;
   cursor: pointer;
+  background-color: darkorange;
+  height: 40px;
+  display: inline-block;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  margin-left: 1325px;
 }
 </style>
