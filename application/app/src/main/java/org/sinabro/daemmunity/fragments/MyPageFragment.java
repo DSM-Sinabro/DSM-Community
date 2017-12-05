2bfeeaec0fbb0be895791eaa994e7848f1558d5c
khg0712 <k7120792@gmail.com>
2017-12-05 17:11:19 +0900

    마이 페이지 완성

--- application/app/src/main/java/org/sinabro/daemmunity/fragments/MyPageFragment.java
+++ application/app/src/main/java/org/sinabro/daemmunity/fragments/MyPageFragment.java
@@ -2,6 +2,8 @@ package org.sinabro.daemmunity.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +12,7 @@ import org.sinabro.daemmunity.R;
 
 
 public class MyPageFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
     public MyPageFragment() {
@@ -28,25 +23,22 @@ public class MyPageFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static MyPageFragment newInstance() {
         MyPageFragment fragment = new MyPageFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_my_page,container,false);
 
+        return v;
+    }
 }
