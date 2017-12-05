510c6c11ab6bd2ef4750c02f3d77b4649affe032
Sumin Go <jennifer045020@gmail.com>
2017-12-05 20:01:49 +0900

    고수민꺼 합침

---
+++ application/app/src/main/java/org/sinabro/daemmunity/activities/Main2Activity.java
@@ -0,0 +1,65 @@
+package org.sinabro.daemmunity.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.sinabro.daemmunity.R;
+
+
+/**
+ * Created by user on 2017-11-07.
+ */
+
+public class Main2Activity extends AppCompatActivity{
+
+    Button projectBtn, contestBtn, studyBtn, clubBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        projectBtn = (Button) findViewById(R.id.projectBtn);
+        contestBtn =(Button) findViewById(R.id.contestBtn);
+        studyBtn = (Button) findViewById(R.id.studyBtn);
+        clubBtn = (Button) findViewById(R.id.clubBtn);
+
+        contestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, RecruitActivity.class);
+                intent.putExtra("statusNum", 1);
+                startActivity(intent);}
+        });
+
+        projectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, RecruitActivity.class);
+                intent.putExtra("statusNum", 2);
+                startActivity(intent);}
+        });
+
+        clubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, RecruitActivity.class);
+                intent.putExtra("statusNum", 3);
+                startActivity(intent);
+            }
+        });
+
+        studyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, RecruitActivity.class);
+                intent.putExtra("statusNum", 4);
+                startActivity(intent);
+            }
+        });
+    }
+}
