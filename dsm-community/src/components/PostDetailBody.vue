809295000ab763ca8443a14220942922bfd4b28a
jieunjieun <kyn07077@gmail.com>
2017-12-14 23:53:51 +0900

    add detail layout

--- dsm-community/src/components/PostDetailBody.vue
+++ dsm-community/src/components/PostDetailBody.vue
@@ -9,9 +9,19 @@
                 </div>
                 <div class = "under_line"></div>
                 <div>
-                  <div class = "author">시발년</div>
-                  <div class = "post_date">2015:15:245</div>
+                  <div class = "author">author</div>
+                  <div class = "post_date">2015:15:24</div>
                 </div>
+                <div class = "hash_tag">
+                  <div class = "tag_zone">
+                    <div class = "tag" v-for="tag in tags" :key="tag.id">
+                      <span class = "tag_title">{{ tag.title }}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class = "main_content">
+                here is main_content
               </div>
             </div>
         </div>
@@ -22,6 +32,14 @@
 import TopBar from './TopBar'
 export default {
   name: 'PostDetailBody',
+  data: function () {
+    return {
+      tags: [
+        {title: 'tags'},
+        {title: 'tags tags'}
+      ]
+    }
+  },
   components: {
     TopBar
   }
@@ -47,6 +65,55 @@ export default {
   .title_text {
     display: inline-block;
     background-color: blue;
+    font-size: 50px;
+    margin-left: 10px;
 
+  }
+
+  .title_box {
+    border: 1px solid black;
+  }
+
+  .author {
+    display: inline-block;
+    margin-left: 10px;
+  }
+
+  .post_date {
+    display: inline-block;
+    margin-left: 10px;
+  }
+
+  .main_content {
+    background-color: red;
+  }
+
+  .hash_tag {
+    height: 45px;
+    width: 730px;
+    border: 1px solid blue;
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .tag {
+    padding-left: 10px;
+    padding-right: 10px;
+    height: 30px;
+    border: 1px solid black;
+    margin-left: 10px;
+    /* display: inline-block; */
+    vertical-align: middle;
+    display: table-cell;
+  }
+
+  .tag_title {
+    margin: auto;
+    display: inline-block;
+  }
+
+  .tag_zone {
+    display: inline-block;
+    margin-left: 10px;
   }
 </style>
