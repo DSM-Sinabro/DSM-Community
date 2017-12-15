107c488539804a5979c2342f0cdf11756585ea4f
yjh5424 <yjh54240@naver.com>
2017-12-15 19:20:03 +0900

    [안드로이드] 바텀시트 추가

--- server/routes/comment/index.js
+++
@@ -1,9 +0,0 @@
-const controller = require('./comment.controller');
-let router = require('express').Router();
-
-
-router.route('/comment').post(controller.postComment);
-router.route('/comment/:commentId').put(controller.reviseComment);
-router.route('/comment/:commentId').delete(controller.dropComment)
-
-module.exports = router;
