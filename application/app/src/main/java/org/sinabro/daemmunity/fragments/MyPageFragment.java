086bbec58ac839766c9994b3cca3e56b623b0c4b
yjh5424 <yjh54240@naver.com>
2017-12-15 19:09:33 +0900

    마이페이지 수정

--- application/app/src/main/java/org/sinabro/daemmunity/fragments/MyPageFragment.java
+++ application/app/src/main/java/org/sinabro/daemmunity/fragments/MyPageFragment.java
@@ -1,6 +1,9 @@
 package org.sinabro.daemmunity.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.sinabro.daemmunity.R;
+import org.sinabro.daemmunity.activities.LoginActivity;
 
 
 public class MyPageFragment extends Fragment {
@@ -37,8 +41,39 @@ public class MyPageFragment extends Fragment {
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_my_page,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_my_page,container,false);
+        nextLogin(rootView);
+        showBottomSheet(rootView);
 
-        return v;
+
+
+        return rootView;
+    }
+
+    public void nextLogin(View rootView){
+        View loginView=rootView.findViewById(R.id.loginLayout);
+
+        loginView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void showBottomSheet(View rootView){
+        final BottomSheetDialog dialog= new BottomSheetDialog(getActivity());
+
+        FloatingActionButton button=rootView.findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View sheet=getActivity().getLayoutInflater().inflate(R.layout.view_bottom_sheet, null);
+                dialog.setContentView(sheet);
+                dialog.show();
+            }
+        });
+
     }
 }
