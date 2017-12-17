b2dd8475d39146c2aa4c896b9286e508b8e84e23
unknown <geon0250@gmail.com>
2017-12-17 23:46:53 +0900

    글 리스트 페이지 태그 수정

--- dsm-community/src/components/BoardBody/BoardMain.vue
+++ dsm-community/src/components/BoardBody/BoardMain.vue
@@ -9,9 +9,7 @@
             <h4 id="date">{{ notice.date }}</h4>
         </div>
         <div>
-            <div id="tag"><p>{{ notice.tag }}</p></div> <!-- 날짜 이름 조회수 답글 위치 수정 -->
-            <div id="tag"><p>{{ notice.tag2 }}</p></div>
-            <div id="tag"><p>{{ notice.tag3 }}</p></div>
+            <hash-tag :tags="notice.tags" id="tags"/>
             <div id="view_box">
                <icon id="eye" name="eye"></icon>
                <p id="text">{{ notice.views }}</p>
@@ -23,88 +21,89 @@
 </template>
 
 <script>
+import HashTag from '../HashTag'
 export default {
   name: 'BoardMain',
-  props: ['notice']
+  props: ['notice'],
+  components: {
+    HashTag
+  }
 }
 </script>
 
 
 <style>
-    #text_title{
-        color: gray;
-        float: left;
-    }
-    #box{
-        z-index: 2;
-        width: 45%;
-        height: 100px;
-        border: 3px solid orange;
-        margin-left: 27.5%;
-        padding-top: 15px;
-        padding-left: 20px;
-        margin-bottom: 20px;
-        cursor:pointer;
-    }
-    #circle{
-        border-radius: 100%;
-        background-color: gray;
-        width: 20px;
-        height: 20px;
-        margin-top: -1px;
-        margin-right: 6px; 
-        float: left;
-    }
-    #name{
-        color: gray;
-        font-weight: bold;
-    }
-    #name_box{
-        width: 20%;
-        height: 20%;
-        margin-left: 75%;
-    }
+#text_title{
+    color: gray;
+    float: left;
+}
 
-    #date_box{
-        width: 40%;
-        height: 20%;
-        margin-left: 75%;
-        clear: both;
-    }
-    #date{
-        color: gray;
-    }
-    #tag{
-        width: 70px;
-        height: 25px;
-        background-color: coral;
-        color: white;
-        text-align: center;
-        float: left;
-        margin-right: 10px;
-        font-size: 12px;
-        padding-top: 5px;
-    }
-    #view_box{
-        width: 200px;
-        height: 25px;
-        float: left;
-    }
-    #eye{
-        width: 27px;
-        height: 27px;
-        float: left;
-        margin-left: 50px;
-    }
-    #reply{
-        width: 25px;
-        height: 25px;
-        float: left;
-    }
-    #text{
-        float: left;
-        margin-left: 2px;
-        margin-right: 20px; 
-        margin-top: 4px;
-    }
+#box{
+    z-index: 2;
+    width: 45%;
+    height: 130px;
+    border: 3px solid orange;
+    margin-left: 27.5%;
+    padding: 20px;
+    margin-bottom: 20px;
+    cursor: pointer;
+}
+
+#circle{
+    border-radius: 100%;
+    background-color: gray;
+    width: 20px;
+    height: 20px;
+    margin-top: -1px;
+    margin-right: 6px; 
+    float: left;
+}
+
+#name{
+    color: gray;
+    font-weight: bold;
+}
+
+#name_box{
+    width: 20%;
+    height: 20%;
+    margin-left: 75%;
+}
+
+#date_box{
+    width: 40%;
+    height: 20%;
+    margin-left: 75%;
+    clear: both;
+}
+#date{
+    color: gray;
+}
+
+#tags {
+    display: inline-block;
+}
+
+#view_box{
+    width: 200px;
+    height: 25px;
+    display: inline-block;
+}
+#eye{
+    width: 27px;
+    height: 27px;
+    float: left;
+    margin-left: 50px;
+}
+#reply{
+    width: 25px;
+    height: 25px;
+    float: left;
+}
+#text{
+    float: left;
+    margin-left: 2px;
+    margin-right: 20px; 
+    margin-top: 4px;
+}
 </style>
--- dsm-community/src/components/PostBody.vue
+++ dsm-community/src/components/PostBody.vue
@@ -33,11 +33,11 @@ export default {
   data: function () {
     return {
       notices: [
-        {title: 'HELLO', image: '1234', name: '박해빈', date: '2017/11/08/FRI', views: '123', reply: '123', tag: '#HELLO', tag2: '#', tag3: '#'},
-        {title: '체육대회', image: '1234', name: '장필준', date: '2017/11/08/FRI', views: '123', reply: '123', tag: '#배구', tag2: '#줄다리기', tag3: '#예선'},
-        {title: 'DSM 공모전', image: '13123', name: '한능희', date: '2018/12/31/TUE', views: '123', reply: '123', tag: '#힘내라', tag2: '#영상제출', tag3: '#11.15'},
-        {title: '자치회 회의 초대', image: '123', name: '박해빈', date: '2010/12/12/TUE', views: '123', reply: '123', tag: '#윤정현', tag2: '#박해빈', tag3: '#자치회'},
-        {title: '학교 규정', image: '1232', name: '서현철', date: '2010/12/12/TUE', views: '123', reply: '122', tag: '#동복', tag2: '#사복안됨', tag3: '#춥다'}
+        {title: 'HELLO', image: '1234', name: '박해빈', date: '2017/11/08/FRI', views: '123', reply: '123', tags: [{title: 'hi'}, {title: 'hello'}, {title: 'hihihi'}]},
+        {title: '체육대회', image: '1234', name: '장필준', date: '2017/11/08/FRI', views: '123', reply: '123', tags: [{title: 'hi'}, {title: 'hello'}, {title: 'hihihi'}]},
+        {title: 'DSM 공모전', image: '13123', name: '한능희', date: '2018/12/31/TUE', views: '123', reply: '123', tags: [{title: 'hi'}, {title: 'hello'}, {title: 'hihihi'}]},
+        {title: '자치회 회의 초대', image: '123', name: '박해빈', date: '2010/12/12/TUE', views: '123', reply: '123', tags: [{title: 'hi'}, {title: 'hello'}, {title: 'hihihi'}]},
+        {title: '학교 규정', image: '1232', name: '서현철', date: '2010/12/12/TUE', views: '123', reply: '122', tags: [{title: 'hi'}, {title: 'hello'}, {title: 'hihihi'}]}
       ]
     }
   },
